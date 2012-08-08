378aed81bf656f1a2c0650c1da9a2c65a543c040
Taran <haohongting@gmail.com>
2012-08-08 09:07:13 +0800

    Update NWImage/NWImage_SDL.cpp

--- NWImage/NWImage_SDL.cpp
+++ NWImage/NWImage_SDL.cpp
@@ -7,18 +7,18 @@ bool NWImage_SDL::initWithFile( const char* file )
 {
 	if(m_image)
 	{
-		delete m_image;
+	    return false;
 	}
 	/* load image from file */
 	SDL_RWops* imageFile;
 	imageFile = SDL_RWFromFile(file, "rb");
 	m_image = IMG_Load_RW(imageFile, SDLAutoClose);
 
-	/*init image data*/
+	/* init image data */
 	m_width = m_image->w;
 	m_height = m_image->h;
 	m_data = m_image->pixels;
+	return true;
 }
-
 
 }
