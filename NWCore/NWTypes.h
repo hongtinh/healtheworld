14b499c867ee69049188e3d81a0f9a4f060f0dbe
hongtinh <haohongting@gmail.com>
2012-08-28 21:45:38 +0800

    add class NWEntity

--- NWCore/NWTypes.h
+++ NWCore/NWTypes.h
@@ -16,23 +16,7 @@ namespace NorthWind
 		GLfloat u;
 		GLfloat v;
 	}NWPointTexCoord2F_t ;
-/*
-	typedef struct
-	{
-		NWPointTexCoord2F_t bl;
-		NWPointTexCoord2F_t br;
-		NWPointTexCoord2F_t tl;
-		NWPointTexCoord2F_t tr;
-	}NWTexCoordQuad_T2F_t;
 
-	typedef struct
-	{
-		NWPointVertex2F_t bl;
-		NWPointVertex2F_t br;
-		NWPointVertex2F_t tl;
-		NWPointVertex2F_t tr;
-	}NWVertexQuad_V2F_t;
-*/
 	/* texture point definition */
 	typedef struct {
 		NWPointVertex2F_t		vertices;
--- NWEntity/NWEntity.h
+++ NWEntity/NWEntity.h
@@ -1,12 +1,22 @@
 #ifndef NWENTITY_H
 #define NWENTITY_H
+
+#include "../NWCore/NWTile/NWTile.h"
+
 namespace NorthWind
 {
 
-
 class NWEntity
 {
+public:
+	NWEntity(NWTile* tile, int x, int y):m_pTile(tile),m_x(x),m_y(y) { }
+	~NWEntity();
+	void moveTo(int x, int y) { m_x = x; m_y = y; }
 
+public:
+	NWTile m_pTile;
+	GLfloat m_x;
+	GLfloat m_y;
 };
 
 }
