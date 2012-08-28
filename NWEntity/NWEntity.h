bba2b3f13e69e9f71434621cb0246b3e5af68cea
hongtinh <haohongting@gmail.com>
2012-08-28 21:54:11 +0800

    add height/width for NWEntity

--- NWEntity/NWEntity.h
+++ NWEntity/NWEntity.h
@@ -9,7 +9,9 @@ namespace NorthWind
 class NWEntity
 {
 public:
-	NWEntity(NWTile* tile, int x, int y):m_pTile(tile),m_x(x),m_y(y) { }
+	NWEntity(NWTile* tile, int x, int y, int width, int height):
+	  m_pTile(tile),m_x(x),m_y(y),m_height(height),m_width(width) { }
+
 	~NWEntity();
 	void moveTo(int x, int y) { m_x = x; m_y = y; }
 
@@ -17,6 +19,8 @@ public:
 	NWTile m_pTile;
 	GLfloat m_x;
 	GLfloat m_y;
+	GLfloat m_height;
+	GLfloat m_width;
 };
 
 }
